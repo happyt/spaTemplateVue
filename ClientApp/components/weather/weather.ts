d07515d5ec5e716f23cd483e90648fef531fb88e
ian morton <ian.happytuesdays@gmail.com>
2018-03-01 14:59:39 +0000

    Next button

--- ClientApp/components/weather/weather.ts
+++ ClientApp/components/weather/weather.ts
@@ -9,20 +9,39 @@ interface wItem {
  
 @Component
 export default class WeatherComponent extends Vue {
-    wItem: {} = { "temp": "", "summary": "", "city": ""};
- 
+    wItem: {} = { "temp": "", "summary": "", "city": "" }
+    cities = ["London", "Montreal", "New York"]
+    count: number = 0;
+
     data() {
         return {
             wItem: { "temp": "", "summary": "", "city": ""}
         };
     }
     mounted() {
-        fetch('/api/weather/London')
+        console.log("mounted...", this.count)
+        console.log("value before...", this.cities.length, this.count)
+        this.getData()
+    }
+    getData() {
+        fetch('/api/weather/' + this.cities[this.count])
             .then(response => response.json() as Promise<wItem>)
             .then(data => {
                 this.wItem = data;
             });
-        console.log("mounted...")
+ //       this.count = (this.count >= this.cities.length - 1) ? 0 : this.count+1
+        console.log("values after...", this.cities.length, this.count)
+        
+    }
+    updateCount() {
+        this.count = (this.count >= this.cities.length - 1) ? 0 : this.count+1
+        this.getData()
+    }
+    beforeCreate() {
+        console.log("beforeCreate...")
+    }    
+    created() {
+        console.log("created...")
     }
     beforeUpdate() {
         console.log("beforeUpdate...")
